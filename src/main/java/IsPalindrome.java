62dc1665bd4aa27ac837d231b65b80078af6fe72
PostMaster Pozitron <pmpozitron@gmail.com>
2022-11-28 16:40:23 +0300

    is palindrome for string (previously was done for ints)

--- src/main/java/IsPalindrome.java
+++ src/main/java/IsPalindrome.java
@@ -1,13 +1,38 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class IsPalindrome {
 
     public static void main(String[] args) {
-        for (int i : new int[]{121, -121, 10}) {
-            System.out.println(i + " " + isPalindrome(i));
+//        for (int i : new int[]{121, -121, 10}) {
+//            System.out.println(i + " " + isPalindrome(i));
+//        }
+
+//        for (String s : new String[]{"abc", "abccba", "abcdcba", "", null, "A man, a plan, a canal: Panama"}) {
+        for (String s : new String[]{"A man, a plan, a canal: Panama"}) {
+            System.out.println(s + " " + isPalindrome(s));
         }
     }
 
     public static boolean isPalindrome(int x) {
         var asString = new StringBuilder().append(x);
         return String.valueOf(x).equals(asString.reverse().toString());
+    }
+
+    public static boolean isPalindrome(String s) {
+        if (s==null) return false;
+        if (s=="") return true;
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toLowerCase().toCharArray()) {
+            if (c>='0' && c<='9') sb.append(c);
+            if (c>='a' && c<='z') sb.append(c);
+        }
+
+        for (int i=0,j=sb.length()-1; i<sb.length()||j>=0;i++,j--){
+            if (sb.charAt(i) != sb.charAt(j)) return false;
+        }
+
+        return true;
     }
 }

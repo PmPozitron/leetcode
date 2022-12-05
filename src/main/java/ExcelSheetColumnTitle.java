f638f822ec1e0c71fae3607f89549d4e3e2ede6e
PostMaster Pozitron <pmpozitron@gmail.com>
2022-12-05 17:39:01 +0300

    excel sheet column title #2 (title to number this time)

--- src/main/java/ExcelSheetColumnTitle.java
+++ src/main/java/ExcelSheetColumnTitle.java
@@ -19,4 +19,17 @@ public class ExcelSheetColumnTitle {
     private static char digitToLetter(int digit) {
         return (char)(digit + 'A');
     }
+
+    public static int titleToNumber(String columnTitle) {
+        int result = 0;
+        for (int i = columnTitle.length() - 1, j = 0; i >= 0; i--) {
+            char ch = columnTitle.charAt(i);
+            result+= Math.pow(26, j++) * charToInteger(ch);
+        }
+        return result;
+    }
+
+    private static int charToInteger(char ch) {
+        return ch - 'A' + 1;
+    }
 }

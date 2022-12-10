d0d8b0b6bb1aa956c942a9cecebdb7235ad0ae2e
PostMaster Pozitron <pmpozitron@gmail.com>
2022-12-10 13:33:14 +0300

    my atoi

--- src/main/java/IntegersTasks.java
+++ src/main/java/IntegersTasks.java
@@ -1,8 +1,9 @@
-public class ReverseIntegers {
+public class IntegersTasks {
 
     public static void main(String[] args) {
 //        System.out.println(Integer.toBinaryString(reverseBits(9)));
-        System.out.println(reverseIntegerWoLong(-2147483648));
+//        System.out.println(reverseIntegerWoLong("2147483646"));
+        System.out.println(myAtoi("-2147483647"));
     }
 
     public static int reverseBits(int n) {
@@ -56,5 +57,48 @@ public class ReverseIntegers {
         return n > 0
                 ? current
                 : current * -1;
+    }
+
+    public static int myAtoi(String s) {
+        s = s.trim();
+        if (s.length() == 0) return 0;
+        if (charNotAllowed(s.charAt(0))) return 0;
+
+        int sign = 1;
+        if (s.charAt(0) == '-') sign = -1;
+
+        int result = 0;
+        int next = 0;
+        boolean failFastOn = false;
+        for (char ch : s.toCharArray()) {
+            if (isDigit(ch)) {
+                next = getInteger(ch);
+
+                if ((result * sign > Integer.MAX_VALUE/10 ||
+                        (result * sign == Integer.MAX_VALUE/10 && next > Integer.MAX_VALUE % 10)))
+                    return Integer.MAX_VALUE;
+                if (result * sign < Integer.MIN_VALUE/10 ||
+                        (result * sign == Integer.MIN_VALUE/10 && next > -1*(Integer.MIN_VALUE % 10)))
+                    return Integer.MIN_VALUE;
+
+                result = result * 10 + getInteger(ch);
+            } else if (failFastOn) {
+                break;
+            }
+            failFastOn = true;
+        }
+
+        return result * sign;
+    }
+
+    private static boolean isDigit(char ch) {
+        return ch >= '0' && ch <= '9';
+    }
+    private static int getInteger(char ch) {
+        return ch - '0';
+    }
+
+    private static boolean charNotAllowed(char ch) {
+        return ! (isDigit(ch) || ch == '-' || ch == '+');
     }
 }

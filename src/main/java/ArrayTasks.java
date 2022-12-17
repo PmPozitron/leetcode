f47264c14b1b272fd2388d739f8a6c5f239ed0be
PostMaster Pozitron <pmpozitron@gmail.com>
2022-12-17 21:00:10 +0300

    next greater element 2 (another version of similar looking task)

--- src/main/java/ArrayTasks.java
+++ src/main/java/ArrayTasks.java
@@ -1,6 +1,9 @@
+
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -9,6 +12,7 @@ import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -18,7 +22,8 @@ public class ArrayTasks {
     public static void main(String[] args) {
 //        tasksForContainsNearbyDuplicate().entrySet().forEach(entry -> System.out.println(Arrays.toString(entry.getKey()) + " " + entry.getValue() + " " + containsNearbyDuplicate(entry.getKey(), entry.getValue())));
 
-        System.out.println(Arrays.toString(nextGreaterElement(new int[]{2,4}, new int[]{1,2,3,4})));
+//        System.out.println(Arrays.toString(nextGreaterElement(new int[]{2,4}, new int[]{1,2,3,4})));
+        System.out.println(Arrays.toString(nextGreaterElementsViaStack(new int[]{1,3,4,2})));
     }
 
     public static int majorityElement(int[] nums) {
@@ -49,18 +54,18 @@ public class ArrayTasks {
         // 2. You can use HashMap for finding maximum frequency and returning the element but it takes O(n) space.
         // 3. Best approach is to use maximumVote-Algorithm(Boyer's Moore Voting Algorithm) which count with its current element in O(N) time and O(1)space.
 
-        int count=0;
-        int max=0;
+        int count = 0;
+        int max = 0;
 
-        for(int ele: nums){
-            if(count==0){
-                max=ele;
+        for (int ele : nums) {
+            if (count == 0) {
+                max = ele;
             }
 
-            if(ele==max){
-                count=count+1;
-            }else{
-                count=count-1;
+            if (ele == max) {
+                count = count + 1;
+            } else {
+                count = count - 1;
             }
         }
         return max;
@@ -71,7 +76,7 @@ public class ArrayTasks {
     // and here you can find human-readable explanation of voting algo
     // https://leetcode.com/problems/majority-element-ii/solutions/1098995/explanation-about-boyer-moore-vote/
     public static List<Integer> majorityElementII(int[] nums) {
-        int oneThird = nums.length/3;
+        int oneThird = nums.length / 3;
         LinkedList<Integer> result = new LinkedList<>();
         HashMap<Integer, Integer> weights = new HashMap<>();
 
@@ -87,12 +92,12 @@ public class ArrayTasks {
 
     public static int mostFrequentEven(int[] nums) {
         Map<Integer, Integer> theMap = Arrays.stream(nums)
-                .filter(i -> i%2 == 0)
+                .filter(i -> i % 2 == 0)
                 .mapToObj(Integer::valueOf)
                 .collect(Collectors.toMap(Function.identity(), i -> 1, (anOld, aNew) -> ++anOld));
         int max = theMap.entrySet().stream()
                 .max(Comparator.comparingInt(entry -> entry.getValue()))
-                .orElse(Map.entry(-1,-1))
+                .orElse(Map.entry(-1, -1))
                 .getValue();
         if (max == -1) return max;
 
@@ -103,7 +108,7 @@ public class ArrayTasks {
                 .getKey();
     }
 
-    public static int[] merge(int[] nums1, int m, int[] nums2, int n){
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n) {
         System.arraycopy(nums2, 0, nums1, m, n);
         Arrays.sort(nums1);
 
@@ -111,11 +116,11 @@ public class ArrayTasks {
     }
 
     // beats 100% (runtime) and 86.77% (memory)
-    public static int[] mergeLinearComplexity(int[] nums1, int m, int[] nums2, int n){
+    public static int[] mergeLinearComplexity(int[] nums1, int m, int[] nums2, int n) {
         int j = 0;
-        for (int i=0; i<nums2.length; i++) {
-            for ( ; j<=m; j++) {
-                if (nums2[i]<= nums1[j]) {
+        for (int i = 0; i < nums2.length; i++) {
+            for (; j <= m; j++) {
+                if (nums2[i] <= nums1[j]) {
                     insertAndShift(nums1, j, nums2[i]);
                     j++;
                     m++;
@@ -131,14 +136,14 @@ public class ArrayTasks {
     }
 
     public static void insertAndShift(int[] nums, int position, int val) {
-        for (int i = nums.length-1 ; i>position; i--) {
-            nums[i] = nums[i-1];
+        for (int i = nums.length - 1; i > position; i--) {
+            nums[i] = nums[i - 1];
         }
         nums[position] = val;
 //        System.out.printf("position %d, val %d, array %s\n",position, val, Arrays.toString(nums));
     }
 
-// с собеса ВК.Видео
+    // с собеса ВК.Видео
 // Даны интервалы на отрезке прямой.
 // Каждый интервал задан парой чисел [a, b], которые обозначают левую и правую границу.
 // Напишите функцию, которая выполняет объединение интервалов и возвращает список объединенных интервалов.
@@ -177,9 +182,9 @@ public class ArrayTasks {
             }
         }
 
-        var result = new int[sortedAsList.size()/2][2];
-        for (int i = 1, j = 0; i<sortedAsList.size(); i+=2, j++) {
-            result[j] = new int[]{sortedAsList.get(i-1), sortedAsList.get(i)};
+        var result = new int[sortedAsList.size() / 2][2];
+        for (int i = 1, j = 0; i < sortedAsList.size(); i += 2, j++) {
+            result[j] = new int[]{sortedAsList.get(i - 1), sortedAsList.get(i)};
         }
 
 //        System.out.println(System.nanoTime() - start);
@@ -195,28 +200,29 @@ public class ArrayTasks {
         int maxProfit = 0;
         int currentProfit;
         int leastPrice = Integer.MAX_VALUE;
-        for (int i=0; i<prices.length;i++) {
+        for (int i = 0; i < prices.length; i++) {
             int currentPrice = prices[i];
-            currentProfit = currentPrice-leastPrice;
-            if (currentProfit>maxProfit)
-                maxProfit=currentProfit;
+            currentProfit = currentPrice - leastPrice;
+            if (currentProfit > maxProfit)
+                maxProfit = currentProfit;
 
-            if (currentPrice<leastPrice)
-                leastPrice=currentPrice;
+            if (currentPrice < leastPrice)
+                leastPrice = currentPrice;
         }
         return maxProfit;
     }
 
     private static Map<int[], int[]> inputAndExpectedResultsForPlusOne() {
         return Map.of(
-                new int[]{1,2,3}, new int[]{1,2,4}
-                , new int[]{4,3,2,1}, new int[]{4,3,2,2}
+                new int[]{1, 2, 3}, new int[]{1, 2, 4}
+                , new int[]{4, 3, 2, 1}, new int[]{4, 3, 2, 2}
                 , new int[]{9}, new int[]{10}
         );
     }
+
     public static int[] plusOne(int[] digits) {
         boolean needToGrow = true;
-        for (int i = digits.length-1; i>=0; i--) {
+        for (int i = digits.length - 1; i >= 0; i--) {
             int current = digits[i] + 1;
             if (current < 10) {
                 digits[i] = current;
@@ -227,7 +233,7 @@ public class ArrayTasks {
             }
         }
         if (needToGrow) {
-            int[] result = new int[digits.length +1];
+            int[] result = new int[digits.length + 1];
             System.arraycopy(digits, 0, result, 1, digits.length);
             result[0] = 1;
             return result;
@@ -238,10 +244,10 @@ public class ArrayTasks {
 
     public static int removeDuplicates(int[] array) {
         int removedQuantity = 0;
-        for (int i = 0; i < array.length-1; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             int current = array[i];
             if (current == Integer.MIN_VALUE) break;
-            while(current == array[i+1]) {
+            while (current == array[i + 1]) {
                 shiftForRemoveDuplicates(array, i, removedQuantity);
                 removedQuantity++;
             }
@@ -251,15 +257,15 @@ public class ArrayTasks {
     }
 
     private static void shiftForRemoveDuplicates(int[] array, int index, int removedQty) {
-        for (int i = index; i < array.length-1; i++) {
-            array[i] = array[i+1];
+        for (int i = index; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
         }
-        array[array.length-1-removedQty] = Integer.MIN_VALUE;
+        array[array.length - 1 - removedQty] = Integer.MIN_VALUE;
 
         System.out.println(removedQty + " " + Arrays.toString(array));
     }
 
-    private static int removeElement(int[]nums, int val) {
+    private static int removeElement(int[] nums, int val) {
         int counter = 0;
         for (int i = 0; i < nums.length - counter; i++) {
             while (nums[i] == val && i + counter < nums.length) {
@@ -271,12 +277,12 @@ public class ArrayTasks {
     }
 
     private static void shiftForRemoveElement(int[] nums, int index, int counter) {
-        for (int i = index; i < nums.length-counter-1; i++) {
-            nums[i] = nums[i+1];
+        for (int i = index; i < nums.length - counter - 1; i++) {
+            nums[i] = nums[i + 1];
         }
     }
 
-    public static int searchInsertPosition(int[]nums, int target) {
+    public static int searchInsertPosition(int[] nums, int target) {
         int result = -1;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] >= target) {
@@ -289,14 +295,14 @@ public class ArrayTasks {
                 : result;
     }
 
-    public static int singleNumber(int[]nums) {
+    public static int singleNumber(int[] nums) {
         HashMap<Integer, Boolean> theMap = new HashMap<>();
 
         Arrays.stream(nums)
                 .forEach(i -> theMap.merge(i, Boolean.FALSE, (old, aNew) -> Boolean.TRUE));
 
         return theMap.entrySet().stream()
-                .filter(entry -> ! entry.getValue())
+                .filter(entry -> !entry.getValue())
                 .findFirst()
                 .get()
                 .getKey();
@@ -313,10 +319,11 @@ public class ArrayTasks {
 
         HashSet<Integer> set = new HashSet<>();
         for (int i : nums) {
-            if (! set.add(i)) return true;
+            if (!set.add(i)) return true;
         }
         return false;
     }
+
     public static boolean containsNearbyDuplicate(int[] nums, int k) {
         HashMap<Integer, List<Integer>> theMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
@@ -338,7 +345,7 @@ public class ArrayTasks {
                 Iterator<Integer> inner = entry.getValue().iterator();
                 IntStream.range(0, shift).forEach(i -> inner.next());
 
-                while(inner.hasNext()) {
+                while (inner.hasNext()) {
                     Integer innerInt = inner.next();
                     if (Math.abs(outerInt - innerInt) <= k) return true;
                 }
@@ -348,19 +355,19 @@ public class ArrayTasks {
         return false;
     }
 
-//    https://leetcode.com/problems/contains-duplicate-ii/solutions/61372/simple-java-solution/
+    //    https://leetcode.com/problems/contains-duplicate-ii/solutions/61372/simple-java-solution/
     public boolean containsNearbyDuplicateSlidingWindow(int[] nums, int k) {
         Set<Integer> set = new HashSet<Integer>();
-        for(int i = 0; i < nums.length; i++){
-            if(i > k) set.remove(nums[i-k-1]);
-            if(!set.add(nums[i])) return true;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) set.remove(nums[i - k - 1]);
+            if (!set.add(nums[i])) return true;
         }
         return false;
     }
 
     private static Map<int[], Integer> tasksForContainsNearbyDuplicate() {
         return Map.of(
-                new int[]{1,2,3,1,2,3}, 2
+                new int[]{1, 2, 3, 1, 2, 3}, 2
         );
     }
 
@@ -374,15 +381,15 @@ public class ArrayTasks {
         boolean rangeOpened = false;
         // [-2147483648,-2147483647,2147483647] l=3
         // -2147483648->
-        for (int i = 0; i < nums.length-1; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
             if (range == null) {
                 range = new StringBuilder(String.valueOf(nums[i]));
             }
 
             boolean nextIsBiggerByAtLeastTwo = false;
-            if (nums[i] < 0 && nums[i+1] > 0) nextIsBiggerByAtLeastTwo = true;
-            else if (nums[i] > 0 && nums[i+1] < 0) nextIsBiggerByAtLeastTwo = false;
-            else if (nums[i+1] - nums[i] > 1) nextIsBiggerByAtLeastTwo = true;
+            if (nums[i] < 0 && nums[i + 1] > 0) nextIsBiggerByAtLeastTwo = true;
+            else if (nums[i] > 0 && nums[i + 1] < 0) nextIsBiggerByAtLeastTwo = false;
+            else if (nums[i + 1] - nums[i] > 1) nextIsBiggerByAtLeastTwo = true;
             if (nextIsBiggerByAtLeastTwo) {
                 if (rangeOpened) {
                     range.append(nums[i]);
@@ -403,11 +410,11 @@ public class ArrayTasks {
 
             if (i == nums.length - 2) {
                 if (range == null)
-                    range = new StringBuilder(String.valueOf(nums[i+1]));
+                    range = new StringBuilder(String.valueOf(nums[i + 1]));
                 else if (rangeOpened) {
-                    range.append(nums[i+1]);
+                    range.append(nums[i + 1]);
                 } else {
-                    range.append('-').append('>').append(nums[i+1]);
+                    range.append('-').append('>').append(nums[i + 1]);
                 }
                 result.add(range.toString());
             }
@@ -416,7 +423,9 @@ public class ArrayTasks {
         return result;
     }
 
-    public static int[] nextGreaterElement(int[]nums1, int[]nums2) {
+    //    https://leetcode.com/problems/next-greater-element-i/
+//    stack + map based solution https://leetcode.com/problems/next-greater-element-i/solutions/991295/java-o-n-time-o-n-space-using-monotonic-stack-with-explanation/
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
         if (nums1.length == 0) return nums1;
 
         int[] result = new int[nums1.length];
@@ -440,4 +449,63 @@ public class ArrayTasks {
         return result;
     }
 
+    //    https://leetcode.com/problems/next-greater-element-ii/
+    /*
+    1,8,-1,-100,-1,222,1111111,-111111
+    1 - 8
+    8 - 222
+    -1 - 222?
+    -100 - 222?
+    -1 - 222
+     */
+    public static int[] nextGreaterElements(int[] nums) {
+        Integer[] result = new Integer[nums.length];
+        LinkedList<Integer> list = new LinkedList<>();
+        Arrays.stream(nums).forEach(list::add);
+
+        ListIterator<Integer> forward = null;
+        ListIterator<Integer> reversed = null;
+        for (int i = 0; i < nums.length; i++) {
+            forward = list.listIterator(i + 1);
+            reversed = list.listIterator();
+
+            while (forward.hasNext()) {
+                int j = forward.next();
+                if (j > nums[i]) {
+                    result[i] = j;
+                    break;
+                }
+            }
+            if (result[i] == null) {
+                while (reversed.hasNext() && reversed.nextIndex() < i) {
+                    int j = reversed.next();
+                    if (j > nums[i]) {
+                        result[i] = j;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return Arrays.stream(result).mapToInt(i -> i == null ? -1 : i.intValue()).toArray();
+    }
+
+    // https://leetcode.com/problems/next-greater-element-ii/solutions/1449789/java-damn-easy-to-understand/
+    public static int[] nextGreaterElementsViaStack(int[] nums) {
+        Stack<Integer> stack=new Stack<>();
+        for(int i=nums.length-1;i>=0;i--){
+            stack.push(nums[i]);
+        }
+
+        int greater[]=new int[nums.length];
+        for(int i=nums.length-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=nums[i]){
+                stack.pop();
+            }
+            greater[i]=stack.empty()?-1:stack.peek();
+            stack.push(nums[i]);
+        }
+
+        return greater;
+    }
 }

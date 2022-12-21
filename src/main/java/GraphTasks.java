a31216413ec5b55dc8c61af06ddceda90c6171ae
PostMaster Pozitron <pmpozitron@gmail.com>
2022-12-21 11:59:29 +0300

    Number of Provinces

--- src/main/java/GraphTasks.java
+++ src/main/java/GraphTasks.java
@@ -6,14 +6,67 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public class GraphTasks {
 
     public static void main(String[] args) throws IOException {
 //        System.out.println(validPath(3, new int[][]{{0,1},{1,2},{2,0}}, 0, 2));
 //        System.out.println(validPath(1, new int[][]{}, 0, 0));
-        System.out.println(validPathViaIterativeDfs(10, new int[][]{{0,7},{0,8},{6,1},{2,0},{0,4},{5,8},{4,7},{1,3},{3,5},{6,5}}, 7, 5));
-//        System.out.println(validPathViaBfs(2000, readHugeValidPathTaskFromFile(), 473, 901));
+//        System.out.println(validPathViaIterativeDfs(10, new int[][]{{0, 7}, {0, 8}, {6, 1}, {2, 0}, {0, 4}, {5, 8}, {4, 7}, {1, 3}, {3, 5}, {6, 5}}, 7, 5));
+//        System.out.println(validPathViaAdjacencyMatrix(2000, readHugeValidPathTaskFromFile(), 473, 901));
+
+//        int[][] input = {{1,1,0},{1,1,0},{0,0,1}};
+//        int[][] input = {{1,0,0},{0,1,0},{0,0,1}};
+        int[][] input = {{1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0}, {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0}, {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0}, {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}};
+        System.out.println(findCircleNum(input));
+
+//        for (int i = 0; i < 1000; i++) {
+//            long start = System.nanoTime();
+//            findCircleNum(input);
+//            System.out.println(System.nanoTime()-start);
+//        }
+    }
+
+    /*
+    https://leetcode.com/problems/number-of-provinces/description/
+    https://leetcode.com/problems/number-of-provinces/discussion/comments/1723380
+    Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+    Output: 2
+        0   1   2
+        -   -   -
+    0|  1   1   0
+    1|  1   1   0
+    2|  0   0   1
+     */
+    public static int findCircleNum(int[][] isConnected) {
+        fillAdjacencyMatrix(isConnected);
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        HashSet<Integer> visited = new HashSet<>();
+
+        for (int i = 0; i < isConnected.length; i++) {
+            List<Integer> reachableVertices = graph.get(i);
+            if (reachableVertices == null) {
+                reachableVertices = new ArrayList<>(isConnected.length);
+                graph.put(i, reachableVertices);
+            }
+
+            for (int j = 0; j < isConnected.length; j++) {
+                if (isConnected[i][j] == 0) continue;
+
+                reachableVertices.add(j);
+            }
+        }
+
+        int result = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : graph.entrySet()) {
+            if (visited.add(entry.getKey())) result++;
+            for (Integer reachableVertex: entry.getValue()) {
+                visited.add(reachableVertex);
+            }
+
+        }
+        return result;
     }
 
     /*
@@ -60,7 +113,7 @@ public class GraphTasks {
         HashSet<Integer> visited = new HashSet<>();
         queue.push(source);
 
-        while (! queue.isEmpty()) {
+        while (!queue.isEmpty()) {
             if (queue.peek() == destination) return true;
 
             Integer vertex = queue.pop();
@@ -84,6 +137,11 @@ public class GraphTasks {
             adjacencyMatrix[edges[i][0]][edges[i][1]] = 1;
         }
 
+        fillAdjacencyMatrix(adjacencyMatrix);
+        return adjacencyMatrix[source][destination] == 1;
+    }
+
+    private static void fillAdjacencyMatrix(int[][] adjacencyMatrix) {
         for (int i = 0; i < adjacencyMatrix.length; i++) {
             for (int j = 0; j < adjacencyMatrix[i].length; j++) {
                 if (adjacencyMatrix[i][j] == 1) {
@@ -96,7 +154,6 @@ public class GraphTasks {
                 }
             }
         }
-        return adjacencyMatrix[source][destination] == 1;
     }
 
     private static int[][] readHugeValidPathTaskFromFile() throws IOException {

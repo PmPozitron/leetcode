6b1740e22132d9420e99b4f9ef2e9732b955af01
PostMaster Positron <pmpozitron@gmail.com>
2024-01-24 15:47:51 +0300

    reverse string 2

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -1,18 +1,27 @@
 import java.util.Map;
 
 public class ReverseString {
-
     public static void main(String[] args) {
-        char[] input = {'A',' ','m','a','n',',',' ','a',' ','p','l','a','n',',',' ','a',' ','c','a','n','a','l',':',' ','P','a','n','a','m','a'};
-//        char[] input = {'a', 'b', 'c', 'd'};
-        System.out.println(input);
-        reverseString(input);
-        System.out.println(input);
-        System.out.println(new char[]{'a','m','a','n','a','P',' ',':','l','a','n','a','c',' ','a',' ',',','n','a','l','p',' ','a',' ',',','n','a','m',' ','A'});
+        // char[] input = {
+        //     'A', ' ', 'm', 'a', 'n', ',', ' ', 'a', ' ', 'p', 'l', 'a', 'n', ',', ' ', 'a', ' ', 'c', 'a', 'n', 'a',
+        //     'l', ':', ' ', 'P', 'a', 'n', 'a', 'm', 'a'
+        // };
+        // //        char[] input = {'a', 'b', 'c', 'd'};
+        // System.out.println(input);
+        // reverseString(input);
+        // System.out.println(input);
+        // System.out.println(new char[] {
+        //     'a', 'm', 'a', 'n', 'a', 'P', ' ', ':', 'l', 'a', 'n', 'a', 'c', ' ', 'a', ' ', ',', 'n', 'a', 'l', 'p',
+        //     ' ', 'a', ' ', ',', 'n', 'a', 'm', ' ', 'A'
+        // });
+        //
+        // Map.of(1, 2);
+        // Map.entry(1, 2);
 
-        Map.of(1, 2);
-        Map.entry(1, 2);
+        // System.out.println(reverseStr("abcdefg", 2));
+        System.out.println(reverseStr("abcd", 2));
     }
+
     /*
     https://leetcode.com/problems/reverse-string/
     abc
@@ -25,5 +34,33 @@ public class ReverseString {
             s[i] = s[s.length - 1 - i];
             s[s.length - 1 - i] = c;
         }
+    }
+
+    /*
+    https://leetcode.com/problems/reverse-string-ii/description/
+    Example 1:
+    Input: s = "abcdefg", k = 2
+    Output: "bacdfeg"
+
+    Example 2:
+    Input: s = "abcd", k = 2
+    Output: "bacd"
+     */
+    public static String reverseStr(String s, int k) {
+        StringBuilder result = new StringBuilder(s);
+
+        for (int i = 0; i < s.length(); i+=2*k) {
+            int left = i;
+            int right = (i + k) < s.length()
+                ? i + k - 1
+                : s.length() - 1;
+
+            while (left < right) {
+                char temp = result.charAt(left);
+                result.setCharAt(left++, result.charAt(right));
+                result.setCharAt(right--, temp);
+            }
+        }
+        return result.toString();
     }
 }
